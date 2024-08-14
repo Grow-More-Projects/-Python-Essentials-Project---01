8149432d79a09c21fd19ed38ce4f72702dd3e428
Coder-Adi-dev <adiprofitcoder@gmail.com>
2024-08-14 13:51:41 +0530

    Create main.py
    Solution

---
+++ main.py
@@ -0,0 +1,9 @@
+# Taking input form user
+num_1 = int(input("Enter a number: "))
+num_2 = int(input("Enter another number: "))
+
+# Giving output to user
+print(f"{num_1} + {num_2} = {num_1 + num_2}")
+print(f"{num_1} - {num_2} = {num_1 - num_2}")
+print(f"{num_1} * {num_2} = {num_1 * num_2}")
+print(f"{num_1} / {num_2} = {num_1 / num_2}")
